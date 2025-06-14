6f3c8025feb71df98cebf5f2bc2a76220e640fe1
Anjali Agarwal <anjaliagarwalkanota@gmail.com>
2025-06-14 10:21:57 +0000

    config dev container

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 454545
+let accountEmail = "anjaliagarwal230705@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+
+
+//accountId = 343434 // not poosible to assign value in const variable
+//console.log(accountId);
+accountEmail = "aagarwal@gmail.com"
+accountPassword = "789012"
+accountCity = "Gurgaon"
+accountState = "Rajasthan";
+console.table([accountEmail , accountPassword , accountCity, accountState])
