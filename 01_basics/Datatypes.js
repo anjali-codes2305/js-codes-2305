eebe1d88978b88eeeac7d6d748586e882d97111d
Anjali Agarwal <anjaliagarwalkanota@gmail.com>
2025-06-15 14:45:09 +0000

    configuration

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using node js , not browser
+let name = "Anjali"
+let age = 34
+let isLoggedIn = false
+
+
+//types of datatypes
+
+//primitive
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null=> stsndalone value
+//undefined => value till not assigned
+//symbol => to find the uniqueness
+
+//object
+console.log(typeof "Anjali")
+console.log(typeof null)
+
+
